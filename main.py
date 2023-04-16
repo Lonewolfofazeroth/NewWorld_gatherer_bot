adee5f848fe254a5b99f58e15c063046d7a19e48
Pomogadryg <denver1033@gmail.com>
2023-04-17 00:29:50 +0300

    started with gatherer

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import pyautogui as pg
+import time
+from pynput.keyboard import Key, Controller
 
+
+keyboard=Controller()
+def conf(img=None):
+    match_=True
+    for c in range(10,6,-1):
+        c = c / 10
+        match_=pg.locateCenterOnScreen(img,confidence=c)
+        if match_:
+            print(c)
+            return(match_)
+    return(None)
+
+while True:
+    # time.sleep(1)
+    while conf("img/E.png"):
+        keyboard.tap("e")
+    keyboard.tap("q")
+    
